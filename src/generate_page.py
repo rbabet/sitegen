2a71d0d700d5e040a7d73765be6c6addc1ece444
rbabet <rbabet@gmail.com>
2025-07-13 16:55:17 -0500

    Changed main.py to use docs instead of public and added CLI parm opt to specify site root

--- src/generate_page.py
+++ src/generate_page.py
@@ -4,7 +4,7 @@ from blocktype import markdown_to_html_node
 from extract_title import extract_title
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path) as md_file:
@@ -18,12 +18,14 @@ def generate_page(from_path, template_path, dest_path):
 
     template_lines = template_lines.replace('{{ Title }}', title)
     template_lines = template_lines.replace('{{ Content }}', md_html)
+    template_lines = template_lines.replace('href="/', f"href=\"{basepath}")
+    template_lines = template_lines.replace('src="/', f"src=\"{basepath}")
 
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, 'w') as target_file:
         target_file.write(template_lines)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     cur_dir = os.listdir(dir_path_content)
     for item in cur_dir:
         new_source = os.path.join(dir_path_content, item)
@@ -31,9 +33,9 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         if os.path.isfile(new_source):
             if new_source.endswith(".md"):
                 new_dest = os.path.splitext(new_dest)[0] + ".html"
-                generate_page(new_source, template_path, new_dest)
+                generate_page(new_source, template_path, new_dest, basepath)
         else:
-            generate_pages_recursive(new_source, template_path, new_dest)
+            generate_pages_recursive(new_source, template_path, new_dest, basepath)
 
 
     
--- src/main.py
+++ src/main.py
@@ -1,24 +1,28 @@
 import os
 import shutil
+from sys import argv
 
 from copystatic import copy_files_recursive
 from generate_page import generate_page, generate_pages_recursive
 
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 
 
 def main():
-    print("Deleting public directory...")
+    basepath = "/"
+    if len(argv) > 1:
+        basepath = argv[1]
+    print(f"Deleting {dir_path_public} directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
-    print("Copying static files to public directory...")
+    print(f"Copying static files to {dir_path_public} directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    #generate_page("content/index.md", "template.html", "public/index.html")
-    generate_pages_recursive("content", "template.html", "public")
+    #generate_page("content/index.md", "template.html", "public/index.html", basepath)
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 if __name__ == "__main__":
     main()
